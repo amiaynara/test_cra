e213c4a0ce42848269065e7a245ac296d70b8acd
Amiay Narayan <amiaynarayan@mac.local>
2022-07-30 15:55:25 +0530

    router error testing

---
+++ push.sh
@@ -0,0 +1,5 @@
+git status
+git add .
+git commit -m 'router error testing'
+git push origin master
+
--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Some heading</h1>
-        <Router basename="/test_cra">
+        <Router>
           <Routes>
             <Route exact path="/login" element={<LoginView />} />
           </Routes>
